2673dc7c320db11d8cd770223ef1da76d122d666
user <user@users-air.home>
2023-09-16 17:03:08 +0100

    Load cities from a file

---
+++ cities1.js
@@ -0,0 +1,8 @@
+var cityData = [
+  { "name": "Paris", "latitude": 48.8567, "longitude": 2.351 },
+  { "name": "Toronto", "latitude": 43.8163, "longitude": -79.4287 },
+  { "name": "Los Angeles", "latitude": 34.3, "longitude": -118.15 },
+  { "name": "Havana", "latitude": 23, "longitude": -82 }
+];
+
+
