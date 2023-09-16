14f2213ad4d8bf01f5b008424700e577528d58d7
user <user@users-air.home>
2023-09-16 17:35:09 +0100

    cleaning

--- cities1.js
+++ cities1.js
@@ -1,4 +1,4 @@
-var cityData = [
+var cityData1 = [
   { "name": "Paris", "latitude": 48.8567, "longitude": 2.351 },
   { "name": "Toronto", "latitude": 43.8163, "longitude": -79.4287 },
   { "name": "Los Angeles", "latitude": 34.3, "longitude": -118.15 },
